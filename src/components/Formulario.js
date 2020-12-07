74849b52d48df1544307f94139fcc33caf55b258
Luis García <aig.luis@gmail.com>
2020-12-07 13:17:56 -0600

    Agregados Proptypes

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Formulario.module.css";
 import useSelect from "../hooks/useSelect";
+import PropTypes from "prop-types";
 
 const Formulario = ({ guardarCategoria }) => {
   //opciones
@@ -40,6 +41,10 @@ const Formulario = ({ guardarCategoria }) => {
       </div>
     </div>
   );
+};
+
+Formulario.propTypes = {
+  guardarCategoria: PropTypes.func.isRequired,
 };
 
 export default Formulario;
--- src/hooks/useSelect.js
+++ src/hooks/useSelect.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 const useSelect = (stateInicial, opciones) => {
   //state del custom hook
@@ -19,6 +20,11 @@ const useSelect = (stateInicial, opciones) => {
   );
 
   return [state, SelectNoticias];
+};
+
+useSelect.propTypes = {
+  stateInicial: PropTypes.string.isRequired,
+  opciones: PropTypes.array.isRequired,
 };
 
 export default useSelect;
